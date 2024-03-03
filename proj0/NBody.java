03466d5bf671d8e8179b0bdd35dd4a1d8e0c103a
kalin <kalinplus@outlook.com>
2024-03-03 17:35:56 +0800

    proj0 completed 2

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -62,12 +62,12 @@ public class NBody {
         }
 
         // print final state
-        StdOut.printf("%d\n", len);
-        StdOut.printf("$.2e\n", uniRadius);
-        for (int i = 0; i < len; i++) {
-            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %12s\n",
-            planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-            planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                          planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                          planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
         }
     }
 }
