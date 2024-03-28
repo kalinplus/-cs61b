06e9684661d264feeded026bd0808160d164715d
kalin <kalinplus@outlook.com>
2024-03-28 22:26:09 +0800

    hw2 complete

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,93 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ *  1. I use boolean[][] to track open, and WQU to check full
+ *  2. But use only 1 WQU resulting in backwash, which is due to indirect link.
+ *     So we use 2 WQUs
+ *    a. 1 with just top site, to check if one site is full
+ *    b. 1 with top and bottom sites, to check if is prelocated
+ *  3. Some other attributes include topSite, bottomSite and nubmerOfOpenSites
+ */
 public class Percolation {
+    private boolean[][] isSitesOpen;
+    private WeightedQuickUnionUF fullUf;
+    private WeightedQuickUnionUF percoUf;
+    int topSite;
+    int botSite;
+    int numOfOpenSite;
+    int size;
 
+    public Percolation(int N) {
+        topSite = N * N;
+        botSite = N * N + 1;
+        numOfOpenSite = 0;
+        size = N;
+
+        isSitesOpen = new boolean[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                isSitesOpen[i][j] = false;
+            }
+        }
+
+        fullUf = new WeightedQuickUnionUF(N * N + 1);
+        percoUf = new WeightedQuickUnionUF(N * N + 2);
+        for (int i = 0; i < N; i += 1) {
+            fullUf.union(topSite, i);
+            percoUf.union(topSite, i);
+            percoUf.union(botSite, N * N - i - 1);
+        }
+    }
+    public void open(int row, int col) {
+        boundCheck(row, col);
+        // handle open
+        isSitesOpen[row][col] = true;
+        numOfOpenSite += 1;
+        // deal with union (full)
+        if (row > 0)
+            unionForOpenSite(row, col, row - 1, col);
+        if (row < size - 1)
+            unionForOpenSite(row, col, row + 1, col);
+        if (col > 0)
+            unionForOpenSite(row, col, row, col - 1);
+        if (col < size -1)
+            unionForOpenSite(row, col, row, col + 1);
+    }
+
+    private void unionForOpenSite(int row, int col, int urow, int ucol) {
+        if (isSitesOpen[urow][ucol]) {
+            fullUf.union(rcToIdx(row, col), rcToIdx(urow, ucol));
+            percoUf.union(rcToIdx(row, col), rcToIdx(urow, ucol));
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        boundCheck(row, col);
+        return isSitesOpen[row][col];
+    }
+    public boolean isFull(int row, int col) {
+        boundCheck(row, col);
+        return isSitesOpen[row][col] && fullUf.connected(rcToIdx(row, col), topSite);
+    }
+    public int numberOfOpenSites() {
+        return numOfOpenSite;
+    }
+
+    public boolean percolates() {
+        return percoUf.connected(topSite, botSite);
+    }
+
+    public static void main(String[] args) {
+    }
+
+    private void boundCheck(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private int rcToIdx(int row, int col) {
+        return row * size + col;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,44 @@
 package hw2;
 
+import java.lang.*;
 public class PercolationStats {
+    private double confidenceConst = 1.96;
+    public double miu;
+    public double sigma;
+    public double confidenceLow;
+    public double confidenceHigh;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        double[] xi = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            Percolation p = pf.make(N);
+            int openSites = 0;
+            while(!p.percolates()) {
+                int randomx = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                int randomy = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                p.open(randomx, randomy);
+                openSites += 1;
+            }
+            xi[i] = (double)(openSites) / (N * N);
+        }
 
+        miu = mean(xi);
+        sigma = stddev(xi);
+        confidenceLow = confidenceLow(miu, sigma, T);
+        confidenceHigh = confidenceHigh(miu, sigma, T);
+    }
+    public double mean(double[] xi) {
+        return edu.princeton.cs.introcs.StdStats.mean(xi);
+    }
+    public double stddev(double[] xi) {
+        return edu.princeton.cs.introcs.StdStats.stddev(xi);
+    }
+    public double confidenceLow(double miu, double sigma, int T) {
+        return miu - (confidenceConst * sigma) / Math.sqrt(T);
+    }
+    public double confidenceHigh(double miu, double sigma, int T) {
+        return miu + (confidenceConst * sigma) / Math.sqrt(T);
+    }
 }
