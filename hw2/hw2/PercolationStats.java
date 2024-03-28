df1b08c224c6fdea1c86e3a7126f6d1d62d022ba
kalin <kalinplus@outlook.com>
2024-03-28 23:12:21 +0800

    hw2 complete

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,38 +3,40 @@ package hw2;
 import java.lang.*;
 public class PercolationStats {
     private double confidenceConst = 1.96;
+    private double[] xi;
+    private int T;
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new IndexOutOfBoundsException();
         }
+        this.T = T;
         double[] xi = new double[T];
         for (int i = 0; i < T; i += 1) {
             Percolation p = pf.make(N);
             int openSites = 0;
             while(!p.percolates()) {
-                int randomx = edu.princeton.cs.introcs.StdRandom.uniform(N);
-                int randomy = edu.princeton.cs.introcs.StdRandom.uniform(N);
-                p.open(randomx, randomy);
+                int x, y;
+                do {
+                    x = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                    y = edu.princeton.cs.introcs.StdRandom.uniform(N);
+                } while (!p.isOpen(x, y));
+
+                p.open(x, y);
                 openSites += 1;
             }
             xi[i] = (double)(openSites) / (N * N);
         }
-
-        mean(xi);
-        stddev(xi);
-        confidenceLow(mean(xi), stddev(xi), T);
-        confidenceHigh(mean(xi), stddev(xi), T);
     }
-    public double mean(double[] xi) {
+    public double mean() {
         return edu.princeton.cs.introcs.StdStats.mean(xi);
     }
-    public double stddev(double[] xi) {
+    public double stddev() {
         return edu.princeton.cs.introcs.StdStats.stddev(xi);
     }
-    public double confidenceLow(double miu, double sigma, int T) {
-        return miu - (confidenceConst * sigma) / Math.sqrt(T);
+    public double confidenceLow() {
+        return mean() - (confidenceConst * stddev()) / Math.sqrt(T);
     }
-    public double confidenceHigh(double miu, double sigma, int T) {
-        return miu + (confidenceConst * sigma) / Math.sqrt(T);
+    public double confidenceHigh() {
+        return mean() + (confidenceConst * stddev()) / Math.sqrt(T);
     }
 }
